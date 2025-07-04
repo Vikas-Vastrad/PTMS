6bad35ddf66197e5d768b3a9c24267a75cac21d5
Vikas-Vastrad <vikasvastrad11@gmail.com>
2025-07-04 12:54:15 +0530

    Created config file

--- src/main/java/com/trackease/timesheet/Auth/AuthRequest.java
+++ src/main/java/com/trackease/timesheet/Auth/AuthRequest.java
@@ -1,4 +1,9 @@
 package com.trackease.timesheet.Auth;
 
+import lombok.Data;
+
+@Data
 public class AuthRequest {
+    private String email;
+    private String password;
 }
--- src/main/java/com/trackease/timesheet/Auth/AuthResponse.java
+++ src/main/java/com/trackease/timesheet/Auth/AuthResponse.java
@@ -1,4 +1,8 @@
 package com.trackease.timesheet.Auth;
 
+import lombok.Data;
+
+@Data
 public class AuthResponse {
+    private String token;
 }
