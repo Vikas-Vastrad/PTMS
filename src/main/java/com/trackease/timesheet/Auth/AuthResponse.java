c918076676865e4bc8f9d6d780ed2c5d435776b1
Vikas-Vastrad <vikasvastrad11@gmail.com>
2025-07-07 15:57:15 +0530

    Edited code for config & auth

--- src/main/java/com/trackease/timesheet/Auth/AuthResponse.java
+++ src/main/java/com/trackease/timesheet/Auth/AuthResponse.java
@@ -1,8 +1,12 @@
 package com.trackease.timesheet.Auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String token;
 }
